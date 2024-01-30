1c6f3dc8a274d8619d6238b187895eed281c9bb7
Hymin2 <khm8246@gmail.com>
2024-01-30 16:03:22 +0900

    feat: index.html에서 로그인 여부에 따라 네비게이션 바 내용 변경
    index.html에서 로그인 시 로그인과 회원가입 메뉴가 사라지고, 사용자의 이름이 보이도록 수정했습니다.

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -3,12 +3,23 @@ package controller;
 import dto.UserResponse;
 import webserver.Model;
 import webserver.annotation.GetMapping;
+import webserver.session.Session;
 
 import java.util.List;
 
 public class HomeController {
     @GetMapping(path = "/index.html")
-    public String indexPage(){
+    public String indexPage(Model model, Session session){
+        if(session != null) {
+            model.setAttributes("login", "");
+            model.setAttributes("register", "");
+            model.setAttributes("userId", session.getUserId() + "님");
+        } else{
+            model.setAttributes("login", "로그인");
+            model.setAttributes("register", "회원가입");
+            model.setAttributes("userId", "");
+        }
+
         return "/index.html";
     }
 }
